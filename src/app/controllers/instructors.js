23783281545b3066b3befb8e37d32fe5245feeea
caique-dev <cpinheroca@gmail.com>
2021-01-11 23:07:44 -0300

    Inserção dos dados do formulário diretamente no BD

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,4 +1,5 @@
 const { age, date } = require('../../lib/utilitarios')
+const db = require('../../config/db')
 
 module.exports = {
     create(req, res) {
@@ -25,24 +26,38 @@ module.exports = {
         for (let key of keys) {
             if (req.body[key] == "") return res.send(`O campo "${key}" está vazio. Por favor, preencha todos os campos.`)
         }
+
+        const query = `
+            INSERT INTO instructors (
+                name,
+                avatar_url,
+                gender,
+                services,
+                birth,
+                created_at
+            ) VALUES ( $1, $2, $3, $4, $5, $6 )
+            RETURNING id, name
+        `
         
-        return res.send('refatorando')
+        const values = [
+            req.body.name,
+            req.body.avatar_url,
+            req.body.gender,
+            req.body.services,
+            date(req.body.birth).ISO,
+            date(Date.now()).ISO
+        ]
+
+        db.query(query, values, (error, results) => {
+            if (error) return res.send('DATABASE error!')
+
+            return res.redirect(`/instructors/${ results.rows[0].id }`)
+        })
     },
     show(req, res) {
         const { id } = req.params
 
-        const foundInstructor = data.instructors.find(instructor.id == id)
-
-        if (!foundInstructor) return res.send('Instrutor não encontrado')
-
-        const instructor = {
-            ...foundInstructor,
-            age: age(foundInstructor.birth),
-            services: foundInstructor.services.split(','),
-            created_at: date(foundInstructor.created_at).desde
-        }
-
-        return res.render('instructors/show', { instructor })
+        return res.render('instructors/show')
     },
     edit(req, res) {
         const { id } = req.params
